ed3f909099173e4d4f1a70370b16374c6e9dcbb3
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-07-18 21:07:54 +0800

    feat: support condition

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,72 +1,82 @@
-import { createElement, Component } from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-import Image from 'rax-image';
-import Header from '../components/Header';
-import WaterFall from '../components/WaterFall';
-import './home.css';
+import { createElement, Component } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import Image from "rax-image";
+import Header from "../components/Header";
+import WaterFall from "../components/WaterFall";
+import "./home.css";
 
-const logo = 'http://gw.alicdn.com/tfs/TB11oXVXRGE3KVjSZFhXXckaFXa-149-148.png';
+const logo = "http://gw.alicdn.com/tfs/TB11oXVXRGE3KVjSZFhXXckaFXa-149-148.png";
 
 export default class extends Component {
   /**
    * Config for page.
    */
   static config = {
-    window: {},
+    window: {}
   };
 
   state = {
-    name: 'Rax',
-    count: 0,
+    name: "Rax",
+    count: 0
   };
 
-  handleClick = (event) => {
+  handleClick = event => {
     this.setState({
-      name: 'MiniApp'
+      name: "MiniApp"
     });
   };
 
   render() {
     // You can access app from page's props.
-    console.log('App:', this.props.app);
+    console.log("App:", this.props.app);
 
     const { name, count } = this.state;
     const source = {
       uri: logo,
       width: 150,
-      height: 150,
+      height: 150
     };
     const obj = {
-      foo: 'bar',
+      foo: "bar",
       a: 1,
-      b:2
+      b: 2
     };
     return (
-      <View x-class={{ 'page-home': true }}>
+      <View x-if={true} x-class={{ "page-home": true }}>
         <Header>
           <Image source={source} className="home-logo" />
-          <Text x-memo className="home-title" onClick={this.handleClick}>Welcome to {name}</Text>
+          <Text x-memo className="home-title" onClick={this.handleClick}>
+            Welcome to {name}
+          </Text>
         </Header>
 
-        <View x-for={(item, key) in obj}>key: {key}, item: {item}</View>
+        <View x-for={(item, key) in obj}>
+          key: {key}, item: {item}
+        </View>
 
         <View x-if={count % 4 === 0}>count: {count}, count % 4 = 0</View>
         <View x-elseif={count % 4 === 1}>count: {count}, count % 4 = 1</View>
         <View x-elseif={count % 4 === 2}>count: {count}, count % 4 = 2</View>
         <View x-else>count: {count}, count % 4 = 3</View>
 
-        <View onClick={() => this.setState((state) => state.count++)}>增加计数</View>
+        <View onClick={() => this.setState(state => state.count++)} style={{background:'red'}}>
+          增加计数
+        </View>
 
         <Fragment>Using fragment tag.</Fragment>
 
         <WaterFall>
           <View x-slot:header>This is header slot content.</View>
-          <View x-slot:item="props">This is item slot content. {JSON.stringify(props)}</View>
+          <View x-slot:item="props">
+            This is item slot content. {JSON.stringify(props)}
+          </View>
           <View x-slot:footer>This is footer slot content.</View>
         </WaterFall>
 
-        <View className="home-intro"><Text>To get started, edit and rebuild.</Text></View>
+        <View className="home-intro">
+          <Text>To get started, edit and rebuild.</Text>
+        </View>
       </View>
     );
   }
--- webpack.config.rewire.js
+++ webpack.config.rewire.js
@@ -1,11 +1,12 @@
 function processConfig(config) {
   const { plugins } = config.module.rules[1].use[0].options;
 
+  plugins.push(require('babel-plugin-transform-jsx-list'));
+  plugins.push(require('babel-plugin-transform-jsx-condition'));
   plugins.push(require('babel-plugin-transform-jsx-memo'));
   plugins.push(require('babel-plugin-transform-jsx-slot'));
   plugins.push(require('babel-plugin-transform-jsx-fragment'));
   plugins.push(require('babel-plugin-transform-jsx-class'));
-  plugins.push(require('babel-plugin-transform-jsx-list'));
 
   return config;
 }
